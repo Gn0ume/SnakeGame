1af9dff3a70f75482294d8fbe5242b89f8820975
Alexander Kravchuk <gn0ume@ya.ru>
2019-02-24 22:26:09 +0300

    Test API hooks

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
-// your code here
+SnakeCanvas.onStart(()=>{
+    console.log('Game Started');
+});
+
+SnakeCanvas.onStop(()=>{
+    console.log('Game Stopped');
+});
+
+SnakeCanvas.onRight(()=>{
+    alert('Turn to the right');
+});
+
+SnakeCanvas.onLeft(()=>{
+    alert('Turn to the left');
+});
+
+SnakeCanvas.onDown(()=>{
+    alert('Turn to down');
+});
+
+SnakeCanvas.onUp(()=>{
+    alert('Turn to up');
+});
+
+console.log(SnakeCanvas.getSnakeSign());
+
+SnakeCanvas.draw(SnakeCanvas.getSnakeSign());
