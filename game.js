b761890638f5a3bb2d41fe1bd4d1aae6fced324e
Alexander Kravchuk <gn0ume@ya.ru>
2019-02-24 22:28:53 +0300

    Food creation, eating, tail cutting, scores setting

--- game.js
+++ game.js
@@ -2,7 +2,6 @@
 const FIELD_SIZE = SnakeCanvas.getSize();
 const DEFAULT_TICK = 300;
 const STEPS_QUEUE_DEEPNESS = 2;
-
 const SNAKE_DIRECTIONS = {
     up: 'up',
     down: 'down',
@@ -15,6 +14,8 @@ const SNAKE_DIRECTIONS = {
         return [this.up, this.down]
     }
 };
+const TICK_INTERVAL = 10;
+
 // variables block
 let tick;
 let intervalID;
@@ -22,6 +23,7 @@ let matrix;
 let bodyQueue;
 let stepsQueue;
 let currentDirection;
+let scores;
 
 drawLogo();
 
@@ -31,6 +33,7 @@ function drawLogo() {
 
 // functions block
 SnakeCanvas.onStart(() => {
+    scores = 0;
     matrix = createFieldMatrix();
     eraseBodyQueue();
     eraseStepsQueue();
@@ -38,6 +41,7 @@ SnakeCanvas.onStart(() => {
     currentDirection = SNAKE_DIRECTIONS.right;
     const currentHeadPosition = initialPosition();
     bodyQueue.push(currentHeadPosition);
+    generateFood();
     beginTicks();
 });
 
@@ -81,7 +85,16 @@ function beginTicks() {
 
 
 function nextStep() {
-    goToNextCell();
+    const nextCell = goToNextCell();
+    if (nextCell === SnakeCanvas.cell_types.food) {
+        SnakeCanvas.setScore(++scores);
+        stopTicks();
+        tick -= TICK_INTERVAL * scores / 100;
+        beginTicks();
+        generateFood();
+    } else {
+        cutTail();
+    }
     drawBody();
     SnakeCanvas.draw(matrix);
 }
@@ -106,6 +119,8 @@ function goToNextCell() {
             break;
     }
     bodyQueue.push(currentHeadPosition);
+    const nextCell = matrix[currentHeadPosition.x][currentHeadPosition.y];
+    return nextCell;
 }
 
 function eraseBodyQueue() {
@@ -163,4 +178,27 @@ function eraseStepsQueue() {
 function turnTo(direction) {
     currentDirection = direction;
     SnakeCanvas.turnSound();
+}
+
+const generateFood = function () {
+    const coordinates = {
+        x: 0,
+        y: 0
+    };
+    do {
+        coordinates.x = randomInteger(0, FIELD_SIZE);
+        coordinates.y = randomInteger(0, FIELD_SIZE);
+    } while (SnakeCanvas.cell_types.body.name === matrix[coordinates.x][coordinates.y].name);
+    matrix[coordinates.x][coordinates.y] = SnakeCanvas.cell_types.food;
+    SnakeCanvas.foodSound();
+};
+
+const randomInteger = (min = 0, max = 10) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+function cutTail() {
+    const tail = bodyQueue[0];
+    matrix[tail.x][tail.y] = SnakeCanvas.cell_types.empty;
+    bodyQueue.shift();
 }
